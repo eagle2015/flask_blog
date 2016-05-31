3abe786864549830ecd16e0bb4c1ac48b12c4921
jwh5566 <jwh5566@foxmail.com>
2016-05-31 18:05:38 +0800

    第六次提交

--- app/entries/blueprint.py
+++ app/entries/blueprint.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*- 
 __author__ = 'jwh5566'
-from flask import Blueprint
+from flask import Blueprint, redirect, request, url_for
 from flask import render_template
 
 from models import Entry, Tag
 from helpers import object_List, entry_List
+from forms import EntryForm
 
 entries = Blueprint('entries', __name__,
                     template_folder='templates')
@@ -27,6 +28,21 @@ def tag_detail(slug):
     tag = Tag.query.filter(Tag.slug == slug).first_or_404()
     entries = tag.entries.order_by(Entry.created_timestamp.desc())
     return entry_List('entries/tag_detail.html', entries, tag=tag)
+
+from blog import db
+@entries.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        form = EntryForm(request.form)
+        if form.validate():
+            entry = form.save_entry(Entry())
+            db.session.add(entry)
+            db.session.commit()
+            return redirect(url_for('entries.detail', slug=entry.slug))
+    else:
+        form = EntryForm()
+    return render_template('entries/create.html', form=form)
+
 
 @entries.route('/<slug>/')
 def detail(slug):
